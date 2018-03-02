6b41b48c7431cc12239fbc632a4426af67e10f13
unknown <supergaoz@qq.com>
2018-03-02 15:42:42 +0800

    init code

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KNN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public static List<List<int>> TrainInputs = null;
+        public static List<int> TrainOutputs = null;
+
+        public static void SetTrainData(List<List<int>> trainInputs, List<int> trainOutputs) {
+            TrainInputs = trainInputs;
+            TrainOutputs = trainOutputs;
+        }
+
+        public static int Predict(List<int> input)
+        {
+            return Predict(input,TrainInputs,TrainOutputs);
+        }
+
+        public static int Predict(List<int> input, List<List<int>> trainInputs, List<int> trainOutputs) {
+
+            return 0;
+        }
+    }
+
+}
---
+++ readme.md
@@ -0,0 +1,5 @@
+﻿## C#实现KNN分类算法
+	相关测试用例与算法实现来自 《机器学习实战》
+
+## KNN using C#
+	test data come from "machine learning in action"
